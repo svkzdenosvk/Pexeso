da790b397ad9a47bc436419f6e3002dcf5bed362
zdeno <zdenko.snurik@gmail.com>
2021-12-01 01:42:54 +0100

    mainly styling max-width of content and little changes in index

--- js/pexeso/index.php
+++ js/pexeso/index.php
@@ -6,47 +6,48 @@ require "functions.php";
 <html lang="en">
     <head>
     <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-8WH83JV9LP"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-8WH83JV9LP"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
-  gtag('config', 'G-8WH83JV9LP');
-</script>
+    gtag('config', 'G-8WH83JV9LP');
+    </script>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
 
         <script type="text/javascript" src="pexeso.js"></script>
-        <!-- <script type="module" src="pexeso.js"></script> -->
 
         <link rel="stylesheet" href="pexeso.css" >
 
         <title>Pexeso</title>
     </head>
     <body>
-    <div>
-        <div class="row_first2">
-            <div id="timeAndStart">
-                <div class="row_first_part_same" id="seconds" >0 s</div>
-                <div class="row_first_part_same" id="start" onclick="timer()">START</div>
-            </div>
-            <div id="up">
-                <div class="row_first_part_same " id="normal"  onclick="setLevel(this)">NORMAL</div>
-                <div class="row_first_part_same " id="harder"  onclick="setLevel(this)">HARDER</div>
-                <div class="row_first_part_same " id="hardest" onclick="setLevel(this)">HARDEST</div>
+        <div>
+            <div class="row_first2">
+                <div id="timeAndStart">
+                    <div class="row_first_part_same" id="seconds" >0 s</div>
+                    <div class="row_first_part_same" id="start" onclick="timer()">START</div>
+                </div>
+                <div id="up">
+                    <div class="row_first_part_same " id="normal"  onclick="setLevel(this)">NORMAL</div>
+                    <div class="row_first_part_same " id="harder"  onclick="setLevel(this)">HARDER</div>
+                    <div class="row_first_part_same " id="hardest" onclick="setLevel(this)">HARDEST</div>
+                </div>
+
             </div>
 
+            <h1>Vstúpili ste do hry Pexeso, najskôr nastavte obtiažnosť</h1>
         </div>
 
-        <h1>Vstúpili ste do hry Pexeso, najskôr nastavte obtiažnosť</h1>
-    </div>
 
-
-    <div class="column_content" id="content">
-        <div class="row" id="row">
-            <?php view_el(_echo_img()); ?>  
-        </div> 
-    </div>
+        <div class="column_content" id="content">
+            <div class="row" id="row">
+                <?php view_el(_echo_img()); ?>  
+            </div> 
+        </div>
 
     </body>
 </html>
